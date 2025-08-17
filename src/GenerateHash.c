9dfaeadc1ec3919edb68a9bf7ab73a1b8a25bce6
mazeed <janujani819@gmail.com>
2025-08-17 15:37:13 +0530

    GenerateHash.c

--- src/GenerateHash.c
+++ src/GenerateHash.c
@@ -1,9 +1,16 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<stdint.h>
-#include"hwkeygen.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <openssl/sha.h>
+#include "macsnatch.h"
 /* function to generate hash */
-uint64_t GenerateHash(const char *hwid){
+unsigned char *GenerateHash(const char *hwid)
+{
+    unsigned char *hash = malloc(SHA512_DIGEST_LENGTH); /* 64 bytes allocated and this array will hold final hash , assume caller calls free*/
+    if (!hash)
+        return NULL; 
+    SHA512((unsigned char *)hwid, strlen(hwid), hash); /* input str, len of str , output array */
 
+    return hash;
 }
