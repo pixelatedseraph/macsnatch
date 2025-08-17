4a289afc25d014f56b32d61fb8b7b8266b3f4762
mazeed <janujani819@gmail.com>
2025-08-17 15:40:03 +0530

    implemented file

--- src/main.c
+++ src/main.c
@@ -1,1 +1,47 @@
-// main source file
+#include <stdio.h>
+#include <stdlib.h>
+#include "macsnatch.h"
+
+int main()
+{
+    /* generate hardware id */
+    char *hwid = GetHWID();
+    if (!hwid)
+    {
+        fprintf(stderr, "failed to generate hardware id\n ");
+        return 1;
+    }
+
+    /* generate hash based on hwid */
+    unsigned char *hash = GenerateHash(hwid);
+    if (!hash)
+    {
+        fprintf(stderr, "failed to generate hash\n ");
+        return 1;
+    }
+
+    /* sign hash to create a license */
+    size_t sig_len;
+    unsigned char *License = SignHash(hash, HASH_LEN, &sig_len);
+    if (!License)
+    {
+        fprintf(stderr, "failed to generate signature\n");
+        return 1;
+    }
+    printf("License Generated, length = %zu bytes \n", sig_len);
+
+    /* verify license  */
+    int Verify = VerifyHash(hash, HASH_LEN, License, sig_len);
+    if (Verify == 1)
+    {
+        printf("License Verfied Succesfully! \n");
+    }
+    else
+    {
+        printf("Invalid license\n");
+    }
+    free(License);
+    free(hash);
+    free(hwid);
+    return 0;
+}
